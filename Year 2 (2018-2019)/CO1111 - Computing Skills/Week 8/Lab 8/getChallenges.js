0f527f9f434463445515e9ebf111338d2803cc17
npanagi <nicolascool@hotmail.com>
2018-11-14 16:31:58 +0200

    Week 8 / Lab 8

--- Year 2 (2018-2019)/CO1111 - Computing Skills/Week 8/Lab 8/getChallenges.js
+++ Year 2 (2018-2019)/CO1111 - Computing Skills/Week 8/Lab 8/getChallenges.js
@@ -3,14 +3,18 @@
 //---------------------//
 
 var xhttp = new XMLHttpRequest();
+
 xhttp.onreadystatechange = function() {
+
     if (this.readyState === 4 && this.status === 200) {
+
         //console.log(this.responseText);
+
         var object = JSON.parse(this.responseText);
+
        for (var i = 0; i < object.treasureHunts.length; i++) {
             console.log(object.treasureHunts[i].name);
         }
-
     }
     else {
         //TODO If response not received (error).
@@ -19,11 +23,10 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "https://codecyprus.org/th/api/list", true);
 xhttp.send();
 
-var myObj = {firstname: "John", lastname: "Smith", age: 30, male: true};
-//Define the data as a JavaScript object.
+var myObj = {firstname: "John", lastname: "Smith", age: 30, male: true}; //Define the data as a JavaScript object.
 
 var myJSON = JSON.stringify(myObj); //Convert it to string using JSON.stringify().
+
 console.log(myJSON); //Print the JSON text to the console.
 
 var challangesList = document.getElementById("challenges");
-//for (var i=0; i < )
