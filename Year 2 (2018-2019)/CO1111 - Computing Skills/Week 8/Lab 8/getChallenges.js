f6bd330aed8c77a372be1a10cb767df2a0dcd7e3
npanagi <nicolascool@hotmail.com>
2018-11-14 16:27:32 +0200

    Week 8 / Lab 8

--- Year 2 (2018-2019)/CO1111 - Computing Skills/Week 8/Lab 8/getChallenges.js
+++ Year 2 (2018-2019)/CO1111 - Computing Skills/Week 8/Lab 8/getChallenges.js
@@ -1,3 +1,7 @@
+//--------------------//
+// FLORI ISE NOOBAS <3//
+//--------------------//
+
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
